99c12a0fc0a97f6ff0082299c82a6f673be2fd41
이호진 <gommind@naver.com>
2023-08-18 16:20:12 +0900

    feat #52 인터페이스 기능 추가 - 인터페이스는 extends를 통해 여러 개 상속받을 수 있습니다. - 클래스도 implements를 통해 여러 개 상속받을 수 있습니다.

---
+++ basicjava/src/oop/polymorphism/InterB.java
@@ -0,0 +1,8 @@
+package oop.polymorphism;
+
+public interface InterB extends SuperInterfaceA, InterBC {
+	
+//	default void show() {
+//		System.out.println("hello");
+//	}
+}
---
+++ basicjava/src/oop/polymorphism/MyClass.java
@@ -0,0 +1,12 @@
+package oop.polymorphism;
+
+
+public class MyClass implements InterB, InterBC {
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
