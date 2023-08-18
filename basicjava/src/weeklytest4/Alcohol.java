d348ebf1df640d93ad0144743591c56d174095a0
이호진 <gommind@naver.com>
2023-08-19 01:23:17 +0900

    feat #54 상속과 static 기능 추가 - Drink와 Alcohol의 관계는 상속관계입니다. - static 메서드는 오버라이딩 되지 않습니다.

---
+++ basicjava/src/weeklytest4/Alcohol.java
@@ -0,0 +1,43 @@
+package weeklytest4;
+
+/**
+ * Alcohol 설명 : 술
+ * 
+ * @author Playdata
+ *
+ */
+public class Alcohol extends Drink {
+
+	double alcper;// 알코올 도수
+
+	// 생성자
+	public Alcohol(String name, int price, int count, double alcper) {
+		super(name, price, count);
+		this.alcper = alcper;
+	}
+
+	// 메서드
+	/**
+	 * 기능 : 타이틀 출력
+	 */
+	@Override
+	public void printTitle() {
+		System.out.println("상품명(도수[%])\t단가\t수량\t금액");
+	}
+	/**
+	 * 기능 : 상품 정보 출력
+	 */
+	@Override
+	public void printData() {
+		System.out.println(super.getName()
+				+ "("
+				+ alcper
+				+ ")\t"
+				+ super.getPrice()
+				+ "\t"
+				+ super.getCount()
+				+ "\t"
+				+ super.getTotalPrice());
+	}
+
+}
