18884731774a7f25a7dd1cb2e90e31b2547ec885
이호진 <gommind@naver.com>
2023-08-23 09:42:43 +0900

    fix #75 버그 수정 - 조건문 기능 버그를 수정하였습니다.

---
+++ basicjava/src/statement/IfTest.java
@@ -0,0 +1,13 @@
+package statement;
+
+public class IfTest {
+	public static void main(String[] args) {
+		int num1 = 100;
+		if(num1>=100)
+			System.out.println("통과");
+			System.out.println("종료");
+		if(num1<100)
+			System.out.println("통과");
+			System.out.println("종료");
+	}
+}
